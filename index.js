18287b8f2df4e081a19d7da6721d6e2bc94ba9b2
SuheL-Roy <60100467+SuheL-Roy@users.noreply.github.com>
2021-06-02 01:00:17 +0600

    crud complete

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const dotenv = require("dotenv")
 const mongoose =  require("mongoose");
 const router = require("./Routes/auth.js");
+const updaterouter = require('./Routes/Users.js')
+
 
 dotenv.config();
 app.use(express.json());
@@ -12,11 +14,13 @@ mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
+    useFindAndModify:false
 
 }).then(console.log("connect to mongoDB"))
 .catch((err) => console.log(err));
 
 app.use("/api/auth", router);
+app.use("/api/users", updaterouter);
 
 
 app.listen("5000", ()=>{
