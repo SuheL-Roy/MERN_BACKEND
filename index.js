b2ff3e3d1adf1009b586c8b930e855e66049b571
SuheL-Roy <60100467+SuheL-Roy@users.noreply.github.com>
2021-06-01 17:05:48 +0600

    upadted

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv")
+const mongoose =  require("mongoose");
+const router = require("./Routes/auth.js");
 
-console.log('welcome to mearn');
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+
+}).then(console.log("connect to mongoDB"))
+.catch((err) => console.log(err));
+
+app.use("/api/auth", router);
+
 
 app.listen("5000", ()=>{
     console.log("server is runing");
